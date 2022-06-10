b0e8c9c6a9d58ca16e80bc5508c1e2fd0c0d96f2
Julio Dutra <jjcutra@gmail.com>
2022-06-10 19:38:38 -0300

    Iniciar teste com locust

--- infra/main.tf
+++ infra/main.tf
@@ -71,6 +71,7 @@ resource "aws_lb_listener" "entradaLoadBalancer" {
   port              = "8000"
   protocol          = "HTTP"
   default_action {
+    type             = "forward"
     target_group_arn = aws_lb_target_group.alvoLoadBalancer.arn
   }
 }
